1cd5de5cb83c2d4afdd0fbf9befa449b60748a8a
Anatoliy Gostev <aegostev@gmail.com>
2017-03-08 22:24:57 +0300

    Added table 'meals'

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -7,6 +8,9 @@ VALUES ('User', 'user@yandex.ru', 'password');
 
 INSERT INTO users (name, email, password)
 VALUES ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2017-02-03 08:00', 'Breakfast', 500, 100000);
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
