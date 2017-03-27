c54f8ab681610602d6b5a7918ccd15083c430abd
Anatoliy Gostev <aegostev@gmail.com>
2017-03-27 23:09:17 +0300

    Method getWithUser is developed in MealService class.

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -2,9 +2,11 @@ package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -53,5 +55,11 @@ public class MealServiceImpl implements MealService {
     public Meal save(Meal meal, int userId) {
         Assert.notNull(meal, "meal must not be null");
         return repository.save(meal, userId);
+    }
+
+    @Override
+    @Transactional
+    public Meal getWithUser(int id, int userId) {
+        return repository.getWithUser(id, userId);
     }
 }
