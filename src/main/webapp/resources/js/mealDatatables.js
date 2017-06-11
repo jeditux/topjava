26078d90fbfd9502a85a9961dfc8a37b13569ef2
Anatoliy Gostev <aegostev@gmail.com>
2017-06-11 22:10:05 +0300

    Meals table over ajax

---
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Анатолий Гостев on 29.05.2017.
+ */
+var ajaxUrl = 'ajax/meals/';
+var datatableApi;
+
+$(function () {
+    datatableApi = $('#mealTable').DataTable({
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "dateTime"
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "defaultContent": "Edit",
+                "orderable": false
+            },
+            {
+                "defaultContent": "Delete",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "asc"
+            ]
+        ]
+    });
+    makeEditable();
+});
